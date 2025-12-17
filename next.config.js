be57dbc380584001221f4ef375d9b6b22f948910
furkan <0furkangolpinar0@gmail.com>
2025-12-17 17:53:18 +0300

    Merge remote changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
     output: 'export',
     images: {
         unoptimized: true,
     },
+=======
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+>>>>>>> 6c9759f444eb78b59ed4f8b774a7d34b10285ab3
 };
 
 module.exports = nextConfig;
